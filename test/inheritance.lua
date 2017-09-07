70ba20837ea5dc6290737ea4cf3a38c093279f22
andri lim <jangko128@gmail.com>
2017-09-07 11:14:18 +0700

    fixes #4, #8

--- test/inheritance.lua
+++ test/inheritance.lua
@@ -3,5 +3,7 @@ local pn = Pineapple.init("nanas", 134)
 
 assert(av.name == "alpukat")
 assert(av.id == 123)
+assert(av:getId() == 123)
 assert(pn.name == "nanas")
 assert(pn.id == 134)
+assert(pn:getId() == 134)
